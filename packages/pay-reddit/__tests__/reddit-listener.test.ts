c08cc59392f3f3bc00801502198cb49b3c6fad08
Marc Schot <info@cryptology.hk>
2019-05-26 14:48:37 -0500

    update: better Reddit platform - event polling instead of while true

--- packages/pay-reddit/__tests__/reddit-listener.test.ts
+++ packages/pay-reddit/__tests__/reddit-listener.test.ts
@@ -1,10 +1,11 @@
 import "jest-extended";
 import { RedditListener } from "../src/";
+const redditListener: RedditListener = new RedditListener();
 
 describe("pay-reddit: RedditListener()", () => {
     describe("listener()", () => {
         it("should be a function", () => {
-            expect(RedditListener.listener).toBeFunction();
+            expect(redditListener.listener).toBeFunction();
         });
     });
 });
