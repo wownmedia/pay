827a26556bc8db1b47da5c016688c8371a01365f
Marc Schot <info@cryptology.hk>
2019-05-27 07:54:53 -0500

    update: better Reddit platform - event polling instead of while true

---
+++ packages/pay-poller/__tests__/poller.test.ts
@@ -0,0 +1,29 @@
+import "jest-extended";
+
+import { Poller } from "../src";
+const poller: Poller = new Poller(1);
+
+describe("pay-poller", () => {
+    describe("Poller: poll", () => {
+        it("should be a function", () => {
+            expect(poller.poll).toBeFunction();
+        });
+    });
+
+    describe("Poller: onPoll", () => {
+        it("should be a function", () => {
+            expect(poller.onPoll).toBeFunction();
+        });
+
+        it("should fire on even poll", async () => {
+            const pollMock = jest.fn(() => {
+                return true;
+            });
+            poller.onPoll(pollMock);
+            poller.poll();
+            await setTimeout(() => {
+                expect(pollMock).toHaveBeenCalled();
+            }, 2);
+        });
+    });
+});
