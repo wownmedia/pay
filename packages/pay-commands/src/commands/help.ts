7ef9ebfa536a16a14d811de5f09aad0bc89bc146
Marc Schot <info@cryptology.hk>
2019-05-29 08:19:56 -0500

    chore: Typedoc additions pay-commands: commands/Help()

--- packages/pay-commands/src/commands/help.ts
+++ packages/pay-commands/src/commands/help.ts
@@ -2,6 +2,11 @@ import { Messenger } from "@cryptology.hk/pay-messenger";
 import { Reply } from "@cryptology.hk/pay-messenger";
 
 export class Help {
+    /**
+     * @dev Retrieve the help message for a command
+     * @param command {string} The command to retrieve the help message for
+     * @returns {Reply} Object with the specific help message to reply to a user per direct message
+     */
     public static getHelp(command: string): Reply {
         return Messenger.helpMessage(command);
     }
