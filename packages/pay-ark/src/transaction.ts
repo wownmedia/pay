ebc2783fa972ade7f35697ce8f0e83888fa79f19
Marc Schot <info@cryptology.hk>
2019-05-29 07:58:31 -0500

    chore: Typedoc additions pay-ark: ArkTransaction()

--- packages/pay-ark/src/transaction.ts
+++ packages/pay-ark/src/transaction.ts
@@ -7,6 +7,17 @@ import { Network } from "./network";
 const arkEcosystemConfig = config.get("arkEcosystem");
 
 export class ArkTransaction {
+    /**
+     * @dev Generate and sign a Transfer transaction for an ArkEcosystem blockchain
+     * @param amount {BigNumber}    The amount to transfer
+     * @param recipientId {string}  The wallet of the receiver of the transfer
+     * @param vendorField {string}  The vendor field
+     * @param fee {BigNumber}       The fee to set for the Transfer
+     * @param seed  {string}        The decrypted seed of the sender's wallet
+     * @param token {string}        The token of the ArkEcosystem blockchain to send the transfer on
+     * @param secondPassphrase {string} Optional decrypted second seed of the sender's wallet
+     * @returns {Promise<any>}      A signed structure of the Transfer
+     */
     public static async generateTransferTransaction(
         amount: BigNumber,
         recipientId: string,
@@ -44,6 +55,11 @@ export class ArkTransaction {
         return transaction.getStruct();
     }
 
+    /**
+     * @dev Retrieve the network config of an ArkEcosystem > v2.4 Blockchain
+     * @param token {string}    The token of the ArkEcosystem blockchain
+     * @returns {Promise<any>}  The config of the blockchain
+     */
     public static async getNetworkConfig(token: string): Promise<any> {
         token = token.toLowerCase();
         try {
@@ -54,6 +70,12 @@ export class ArkTransaction {
         }
     }
 
+    /**
+     * @dev Calculate a timestamp based on an epoch
+     * @param epoch {string} Epoch (e.g. "2019-05-24T11:48:58.165Z")
+     * @returns {number}    The calculated timestamp
+     * @private
+     */
     private static calculateTimestamp(epoch: string): number {
         const epochTime = moment(epoch)
             .utc()
@@ -62,6 +84,12 @@ export class ArkTransaction {
         return Math.floor((now - epochTime) / 1000);
     }
 
+    /**
+     * @dev Retrieve a configured epoch for an ArkEcosystem Blockchain from the config
+     * @param token {string}    The token for the ArkEcosystem blockchain
+     * @returns {string}        Epoch (e.g. "2019-05-24T11:48:58.165Z")
+     * @private
+     */
     private static getArkEcosystemEpochForToken(token: string): string {
         token = token.toLowerCase();
         try {
