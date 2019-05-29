a8e15ce471583d71fd3ae0fff5b64e6b5edf98c0
Marc Schot <info@cryptology.hk>
2019-05-29 08:14:33 -0500

    chore: Typedoc additions pay-commands: commands/Deposit()

--- packages/pay-commands/src/commands/deposit.ts
+++ packages/pay-commands/src/commands/deposit.ts
@@ -4,6 +4,11 @@ import { User } from "@cryptology.hk/pay-user";
 import { Command } from "../";
 
 export class Deposit {
+    /**
+     * @dev Retrieve the deposit address for a user on an ArkEcosystem blockchain
+     * @param command {Command} The parsed command containing the Username and token of the blockchain
+     * @returns {Promise<Reply>} Object containing the reply message to send per direct message to the user
+     */
     public static async getDeposit(command: Command): Promise<Reply> {
         try {
             const address: string = await User.getWalletAddress(command.commandSender, command.token);
