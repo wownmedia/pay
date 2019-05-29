265cd5488d1279979e0892af526a5095c38e2a83
Marc Schot <info@cryptology.hk>
2019-05-29 08:11:52 -0500

    chore: Typedoc additions pay-commands: commands/Balance()

--- packages/pay-commands/src/commands/balance.ts
+++ packages/pay-commands/src/commands/balance.ts
@@ -5,6 +5,12 @@ import { User, Username } from "@cryptology.hk/pay-user";
 import BigNumber from "bignumber.js";
 
 export class Balance {
+    /**
+     * @dev Retrieve the Balance of a user on an ArkEcosystem blockchain
+     * @param user {Username}   The username/platform pair of the user to retrieve the balance for
+     * @param token {string}    The token of the ArkEcosystem blockchain to retrieve the balance on
+     * @returns {Promise<Reply>} Object containing the formatted reply to send to the user per direct message
+     */
     public static async getBalance(user: Username, token: string): Promise<Reply> {
         try {
             const wallet: string = await User.getWalletAddress(user, token);
