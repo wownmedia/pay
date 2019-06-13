2ddcd1bf0866c2f7a131a742fe6020c65cd0379d
Marc Schot <info@cryptology.hk>
2019-06-13 09:10:32 -0500

    update: updates for testing Twitter on remote server

--- packages/pay-twitter/src/twitter-api.ts
+++ packages/pay-twitter/src/twitter-api.ts
@@ -98,20 +98,8 @@ export class TwitterApi {
     public async sendDirectMessage(username: string, message: string): Promise<boolean> {
         try {
             this.twit.post(
-                "direct_messages/events/new.json",
-                {
-                    event: {
-                        type: "message_create",
-                        message_create: {
-                            target: {
-                                recipient_id: "922102309676638208",
-                            },
-                            message_data: {
-                                text: "Hello World!",
-                            },
-                        },
-                    },
-                },
+                "direct_messages/new",
+                { screen_name: "CryptologyHk", text: "hello world! " },
                 (err, data, response) => {
                     console.log("logging data :", data);
                     console.log("logging error :", err);
