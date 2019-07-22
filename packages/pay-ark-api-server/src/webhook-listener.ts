9706b8d46705ce4bbdc9affd85f8f688434d7845
Marc Schot <info@cryptology.hk>
2019-07-22 15:57:04 -0500

    debug: log received response data

--- packages/pay-ark-api-server/src/webhook-listener.ts
+++ packages/pay-ark-api-server/src/webhook-listener.ts
@@ -126,7 +126,7 @@ export class WebhookListener {
             });
             webhookListener.on("/", async data => {
                 try {
-                    Core.logger.info(`Incomming webhook: ${JSON.stringify(data)}`);
+                    Core.logger.info(`Incoming webhook: ${data}`);
                     const authorization =
                         data.hasOwnProperty("headers") && data.headers.hasOwnProperty("authorization")
                             ? data.headers.authorization
