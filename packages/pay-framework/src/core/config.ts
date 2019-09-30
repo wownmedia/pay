78b7a297039a5fe55e2edf03ebd64dfa9ab3e3e9
Marc Schot <info@cryptology.hk>
2019-09-30 15:46:08 -0500

    update: Core.config coverage 100%

--- packages/pay-framework/src/core/config.ts
+++ packages/pay-framework/src/core/config.ts
@@ -8,18 +8,23 @@ export class Config {
         this.loadFromFile(`${envPaths("ark-pay").config}/pay-config.json`);
     }
 
+    /**
+     * @dev Load a configuration from file
+     * @param path {string} the full path to the configuration file
+     */
     public loadFromFile(path: string): void {
         try {
             this.configuration = require(path);
         } catch (e) {
-            logger.warn(`pay-config: Bad configuration: ${e.message}`);
+            logger.warn(`pay-config: Bad configuration file: ${e.message}`);
+            throw e;
         }
     }
 
     /**
      * @dev Retrieve a sub-configuration from file
      * @param subConfig {string}
-     * @returns {T} The subconfiguration requested
+     * @returns {T} The sub-configuration requested
      */
     public get<T = any>(subConfig: string): T {
         return this.configuration[subConfig];
