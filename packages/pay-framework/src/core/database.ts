387abfac642bb63f03d05ae7a9c1f3c8381b1161
Marc Schot <info@cryptology.hk>
2019-09-30 16:11:47 -0500

    update: Core coverage 100%

--- packages/pay-framework/src/core/database.ts
+++ packages/pay-framework/src/core/database.ts
@@ -4,11 +4,11 @@ import { config } from "../core";
 export const readConfig = config.get("database");
 
 export const payDatabase = new Pool({
-    user: readConfig.hasOwnProperty("dbUser") ? readConfig.dbUser : "arkpay",
-    password: readConfig.hasOwnProperty("dbPassword") ? readConfig.dbPassword : "password",
-    database: readConfig.hasOwnProperty("database") ? readConfig.database : "arkpay",
-    host: readConfig.hasOwnProperty("host") ? readConfig.host : "localhost",
-    port: readConfig.hasOwnProperty("port") ? readConfig.port : 5432,
+    user: readConfig.dbUser,
+    password: readConfig.dbPassword,
+    database: readConfig.database,
+    host: readConfig.host,
+    port: readConfig.port,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
